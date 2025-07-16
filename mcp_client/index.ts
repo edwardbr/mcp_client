e65c1abaf93526d2770bdca81b9524aec0eeac31
Edward Boggis-Rolfe <edward@secretarium.com>
2025-07-16 10:58:11 +0000

    initial commit

---
+++ mcp_client/index.ts
@@ -0,0 +1,46 @@
+import { Notifier, LLM, Context, Crypto, HTTP, HttpRequest } from "@klave/sdk/assembly";
+import {FetchInput, FetchOutput, StoreInput, StoreOutput, ErrorMessage, GraphLoadInput, GraphInitExecutionContextInput, InferenceAddPromptInput, ContextNameInput, InferenceComputeInput, InferenceOutput, InferenceAddFrameInput, InferenceGetPiecesInput} from "./types";
+
+const myTableName = "my_storage_table";
+
+/**
+ * @query
+ * @param {FetchInput} input - A parsed input argument
+ */
+export function fetchValue(input: FetchInput): void {
+
+
+// HTTP.requestJson(query: HttpRequest) : void {
+    let value = Ledger.getTable(myTableName).get(input.key);
+    if (value.length === 0) {
+        Notifier.sendJson<ErrorMessage>({
+            success: false,
+            message: `key '${input.key}' not found in table`
+        });
+    } else {
+        Notifier.sendJson<FetchOutput>({
+            success: true,
+            value
+        });
+    }
+}
+
+/**
+ * @transaction
+ * @param {StoreInput} input - A parsed input argument
+ */
+export function storeValue(input: StoreInput): void {
+
+    if (input.key && input.value) {
+        Ledger.getTable(myTableName).set(input.key, input.value);
+        Notifier.sendJson<StoreOutput>({
+            success: true
+        });
+        return;
+    }
+
+    Notifier.sendJson<ErrorMessage>({
+        success: false,
+        message: `Missing value arguments`
+    });
+}
